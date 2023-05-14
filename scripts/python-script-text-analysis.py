b30b94e29da8377a04e07fc7ea7bb30a0df9487e
Dylan Cluyse <dylan.cluyse@student.hogent.be>
2023-05-14 21:30:38 +0200

    update

--- scripts/python-script-text-analysis.py
+++ scripts/python-script-text-analysis.py
@@ -18,18 +18,12 @@ dict = {
 
 total_df = None
 
-"""
-"""
 def get_sentence_length(sentence):
     doc = nlp(sentence)
     return len(doc)
 
-"""
-"""
 pdf_files = [f for f in os.listdir(folder_path)]
 
-"""
-"""
 for pdf in pdf_files:
 
     if pdf.endswith('pdf'):
@@ -56,16 +50,10 @@ for pdf in pdf_files:
         print(f'...{pdf} not a valid file...')
         pass
 
-    
-
-    """
-    """
     full_text = full_text.strip()
     full_text = full_text.replace('\n', ' ')
     lang = detect(full_text)
 
-    """
-    """
     model = dict.get(detect(full_text), dict.get('en'))
     nlp = spacy.load(model)
     doc = nlp(full_text)
@@ -74,9 +62,6 @@ for pdf in pdf_files:
     for sentence in doc.sents:
         sentences.append(str(sentence))
 
-    """
-    Dataframe opbouwen voor een pdf. Hieronder wordt de zin, bron en zinlengte opgeslaan.
-    """
     df = pd.DataFrame(sentences, columns=['sentence'])
     df['source'] = pdf.split('_')[0]
     
@@ -87,32 +72,19 @@ for pdf in pdf_files:
 
     df['sentence_length'] = df['sentence'].apply(get_sentence_length)
 
-
-    """
-    Filteren. Zinnen kleiner dan 3 woord-tokens zijn niet mogelijk. Deze worden verworpen.
-    """
     df = df[df['sentence_length'] > 4]   
 
-
-    """
-    """
     for key in readability.getmeasures("test")['readability grades'].keys():
         df[key] = df['sentence'].apply(lambda x: readability.getmeasures(x)['readability grades'][key])
 
-    """
-    """
     word_usage_cols = readability.getmeasures("test")['word usage'].keys()
     for key in word_usage_cols:
         df[key] = df['sentence'].apply(lambda x: readability.getmeasures(x, lang=lang)['word usage'][key])
 
-    """
-    """
     sentence_beginnings_cols = readability.getmeasures("test")['sentence beginnings'].keys()
     for key in sentence_beginnings_cols:
         df[key] = df['sentence'].apply(lambda x: readability.getmeasures(x, lang=lang)['sentence beginnings'][key])
 
-    """
-    """
     if total_df is None:
         total_df = df
     else:
